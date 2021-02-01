e8c54b26e650f825dad463bf6a6d5704c53783d4
adarsh10jain <47354397+adarsh10jain@users.noreply.github.com>
2021-02-01 10:53:04 +0530

    commit for collectstatic(5)

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'portfolio/static')
+    os.path.join(BASE_DIR,'static')
 ]
-STSTIC_ROOT=os.path.join(BASE_DIR,'assets')
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
