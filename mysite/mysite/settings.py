7678c2a3f244fa7b52040060b789878fa1534658
adarsh10jain <47354397+adarsh10jain@users.noreply.github.com>
2021-02-01 17:53:41 +0530

    commit django-heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,7 @@ STATICFILES_DIRS=[
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
